766bb36a8a1005d9f4b5969f9dfee1c4ab7e964f
jayeshkarvir5 <jayeshkarvir5@gmail.com>
2019-08-15 22:45:27 +0530

    Minor change

--- QandA_webapp/settings/production.py
+++ QandA_webapp/settings/production.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'QandA_webapp.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 db_from_env = dj_database_url.config()
